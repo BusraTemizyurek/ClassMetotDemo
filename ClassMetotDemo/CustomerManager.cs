a36f264296387a338056b874733289292809ec5f
Büşra Temizyürek <busratemizyureek@gmail.com>
2021-01-20 18:47:18 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(int num, Customer[] customers)
+        {
+            Customer customer = new Customer();
+
+            customer.Num = num;
+
+            Console.Write("Eklemek istediğiniz müşterinin T.C. kimlik nosunu giriniz : ");
+            customer.Id = 5;
+
+            Console.Write("Eklemek istediğiniz müşterinin adını giriniz:");
+            customer.Name = Console.ReadLine();
+
+            Console.Write("Eklemek istediğiniz müşterinin soyadını giriniz:");
+            customer.Surname = Console.ReadLine();
+
+            customers[num] = customer;
+
+
+
+            Console.WriteLine(" \n --------Bilgieri girilen kişi müşteri listesine başarıyla eklenmiştir------- \n ");
+
+        }
+
+        public void Remove(Customer[] customers, CustomerManager customerManager)
+        {
+            int num;
+
+            Console.WriteLine(" Silmek istediğiniz müşterinin sırasını giriniz: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            customers[num].Id = 5;
+            customers[num].Name = "";
+            customers[num].Surname = "";
+
+            customerManager.List(customers);
+        }
+
+        public void List(Customer[] customers)
+        {
+            Customer customer = new Customer();
+
+            Console.WriteLine(" \n -----------Müşteri Listesi---------- \n ");
+
+            foreach (Customer customer1 in customers)
+            {
+                Console.WriteLine("Müşteri sırası: " + customer.Num);
+                Console.WriteLine("Müşteri T.C. no: " + customer.Id);
+                Console.WriteLine("Müşterinin ismi: " + customer.Name);
+                Console.WriteLine("Müşterinin soyismi: " + customer.Surname);
+
+            }
+
+            Console.WriteLine("\n --------İşlem sonu---------- \n");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 0;
+
+            CustomerManager customerManager = new CustomerManager();
+
+            Customer[] customers = new Customer[2];
+
+            int i = 0;
+            while (i < customers.Length)
+            {
+                Console.WriteLine((i + 1) + ". Müşterinin bilgilerini giriniz");
+                customerManager.Add(num, customers);
+                num++;
+                i++;
+            }
+
+
+            customerManager.List(customers);
+
+            customerManager.Remove(customers, customerManager);
+
+
+
+        }
+    }
+}
